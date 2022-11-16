3dc7196130f3788352dd1958c95477a52cd2a1d9
A00463084 <Francis.Kuzhippallil@smu.ca>
2022-11-16 12:31:17 -0400

    Added User Profile (Edit and Delete) functionalities

---
+++ Controllers/UserprofileController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using ServiceGo.Models;
+
+namespace ServiceGo.Controllers
+{
+    public class UserprofileController : Controller
+    {
+        public string UpdateUserprofile(Userprofile acc, SqlConnection conn)
+        {
+            string msg = string.Empty;
+            try
+            {
+
+                SqlCommand cmd = new SqlCommand("Update Users set name = '"+acc.name+"', password = '"+acc.password+"', address = '"+acc.address + "', city = '"+acc.city + "', province = '"+acc.province + "', country = '"+acc.country + "', postal_code = '"+acc.postal_code+"', phone = '"+acc.phone+"' where email = '"+acc.email+"'", conn);
+
+                cmd.CommandType = CommandType.Text;
+
+                int k = cmd.ExecuteNonQuery();
+
+
+                if (k > 0)
+                {
+                    msg = "Update User Profile";
+                }
+                else
+                {
+                    msg = "Update Failed, User Account Doesn't Exist";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+
+            return msg;
+
+        }
+
+        public string DeleteUserprofile(Userprofile acc, SqlConnection conn)
+        {
+            string msg = string.Empty;
+            try
+            {
+
+                SqlCommand cmd = new SqlCommand("Delete from  Users where email = '" + acc.email + "'", conn);
+
+                cmd.CommandType = CommandType.Text;
+
+                int k = cmd.ExecuteNonQuery();
+
+
+                if (k > 0)
+                {
+                    msg = "Update User Profile";
+                }
+                else
+                {
+                    msg = "Update Failed, User Account Doesn't Exist";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+
+            return msg;
+
+        }
+    }
+}
---
+++ Models/Userprofile.cs
@@ -0,0 +1,23 @@
+﻿namespace ServiceGo.Models
+{
+    public class Userprofile
+    {
+        public string name { get; set; }
+
+        public string password { get; set; }
+
+        public string email { get; set; }
+
+        public string address { get; set; }
+
+        public string city { get; set; }
+
+        public string province { get; set; }
+
+        public string country { get; set; }
+
+        public string postal_code { get; set; }
+
+        public string phone { get; set; }
+    }
+}
