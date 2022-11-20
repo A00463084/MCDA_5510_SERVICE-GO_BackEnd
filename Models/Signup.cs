b860554dcdc145812f26d240b4a04b63d14927b8
Shreerag Marar <shreerag.krishnakumar.marar@smu.ca>
2022-11-20 15:18:28 -0400

    resolved cors and mapped signup with json value objects

--- Models/Signup.cs
+++ Models/Signup.cs
@@ -1,23 +1,34 @@
-﻿namespace ServiceGo.Models
+﻿using Newtonsoft.Json;
+
+namespace ServiceGo.Models
 {
     public class Signup
     {
+        [JsonProperty("name")]
         public string name { get; set; }
 
+        [JsonProperty("password")]
         public string password { get; set; }
 
+        [JsonProperty("email_address")]
         public string email { get; set; }
 
+        [JsonProperty("address")]
         public string address { get; set; }
 
+        [JsonProperty("city")]
         public string city { get; set; }
 
+        [JsonProperty("province")]
         public string province { get; set; }
 
+        [JsonProperty("country")]
         public string country { get; set; }
 
+        [JsonProperty("postal_code")]
         public string postal_code { get; set; }
 
+        [JsonProperty("mobile_number")]
         public string phone { get; set; }
     }
 }
